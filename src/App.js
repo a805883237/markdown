9274b0a8c8005f841ef6ebfafc23641852364248
a805883237 <a805883237@163.com>
2016-08-03 13:02:58 +0800

    morning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import marked from 'marked';
+import hljs from 'highlight.js';
+
+import MarkSearch from './MarkSearch.js'
 
 class App extends React.Component {
   getChildContext() {
     return {muiTheme: getMuiTheme()};
   }
   render () {
+        marked.setOptions({
+          highlight: function (code) {
+            return hljs.highlightAuto(code).value;
+          }
+        });
+    let xx = '```js\n console.log("这是js代码")\n```';
     return(
       <div>
-
+        {marked('I am using __markdown__.')}
+        <br/>
+        <MarkSearch />
       </div>
     )
   }
